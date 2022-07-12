9317df8529441bc477b37ed178cae7bbacc93009
nitinpaul <84130983+nitinpaul@users.noreply.github.com>
2022-07-13 01:57:21 +0200

    Add files via upload

---
+++ seizures.py
@@ -0,0 +1,32 @@
+seizures = [
+    {
+        'filename': 'SCN-AJ_180910_183823_1001',
+        'sequence': (13539, 13580),
+        'padded_sequence': (13519, 13600),
+    },
+    {
+        'filename': 'SCN-AJ_180911_063946_1003',
+        'sequence': (21040, 21094),
+        'padded_sequence': (21013, 21121),
+    },
+    {
+        'filename': 'SCN-AJ_180912_004151_1006',
+        'sequence': (8284, 8325 ),
+        'padded_sequence': (8264, 8345),
+    },
+    {
+        'filename': 'SCN-AJ_180912_064236_1007',
+        'sequence': (9918, 9956),
+        'padded_sequence': (9900, 9975),
+    },
+    {
+        'filename': 'SCN-AJ_180912_064236_1007',
+        'sequence': (11865, 11914),
+        'padded_sequence': (11840, 11940),
+    },
+    {
+        'filename': 'SCN-AJ_180912_184406_1009',
+        'sequence': (1, 24),
+        'padded_sequence': (1, 50),
+    },
+]
